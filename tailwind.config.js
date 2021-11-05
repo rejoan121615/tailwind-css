f57db8082a3b4c33bc41baf9635d861f5478db16
Mohd Rejoan <wearecrx@gmail.com>
2021-11-05 15:53:22 +0600

    project initialized

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind css config file
+module.exports = {
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {
+      padding: {
+        13: "3.25rem",
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
